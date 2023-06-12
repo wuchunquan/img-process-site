25134e6106f6cfa5452cdde26a4f8fa0dd037253
wuchunquan <744800102@qq.com>
2023-06-13 01:18:20 +0800

    build to docs dir

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
     build:{
         outDir:"docs"
     },
+    base:"/img-process-site",
     plugins: [vue(), vueJsx(), AutoImport('vue',
         {
             'naive-ui': [
